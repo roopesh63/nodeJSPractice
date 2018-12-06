0006fa7e5e9ecd6807811b35adda8da7b802ac09
Roopesh <you@example.com>
2018-12-06 10:57:06 +0530

    added dynamic port listening

--- Express-server/server.js
+++ Express-server/server.js
@@ -1,11 +1,13 @@
 const express = require("express")
 var app = express()
 const hbs = require("hbs") // template engine for injecting dynamic data into pages
+const port = process.env.PORT || 3000
 app.set("view engine","hbs")
 //app.use(express.static(__dirname + path for file)) - for loading external html css files
 hbs.registerPartials(__dirname + "/Views/partials")
+//app.use() refer middleware latera
 hbs.registerHelper("currentYear",()=>{
-  return new Date().getFullYear()
+  return new Date().getFullYear() // to pass in parameters use space beside function in html file
 })
 app.get("/bad",(req,res)=>{
   res.send({
@@ -19,4 +21,4 @@ app.get("/",(req,res)=>{
     message: "Middle Finger",
   })
 })
-app.listen(3000)
+app.listen(port)
